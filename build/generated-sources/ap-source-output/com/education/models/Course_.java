e7f400e55f9cd3748a1d4a567ae6f2c10d25562a
mohamedkhaled16 <eng.mohamed16@yahoo.com>
2016-12-05 02:41:43 +0200

    finals :D

--- build/generated-sources/ap-source-output/com/education/models/Course_.java
+++ build/generated-sources/ap-source-output/com/education/models/Course_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-04T16:57:54")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-05T02:39:25")
 @StaticMetamodel(Course.class)
 public class Course_ { 
 
